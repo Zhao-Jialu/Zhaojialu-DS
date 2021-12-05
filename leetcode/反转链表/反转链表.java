f73bf17fd95dfe94388a7f78c3e6aa3f8d0eca8d
Zhao-Jialu <91457348+Zhao-Jialu@users.noreply.github.com>
2021-12-05 15:57:36 +0800

    Create 反转链表.java

---
+++ leetcode/反转链表/反转链表.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+       return reverse(head,null);
+        }
+    private ListNode reverse(ListNode head,ListNode L){
+        if(head==null)
+        return L;
+        ListNode p=head.next;
+        head.next=L;
+        return reverse(p,head);
+    }
+}
