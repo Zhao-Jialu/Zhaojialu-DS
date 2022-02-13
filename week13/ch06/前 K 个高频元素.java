547f33fabfd2a284128cf3203c837586ef860cff
Zhao-Jialu <91457348+Zhao-Jialu@users.noreply.github.com>
2022-02-13 12:08:26 +0800

    Create 前 K 个高频元素.java

---
+++ week13/ch06/前 K 个高频元素.java
@@ -0,0 +1,19 @@
+class Solution {
+       public int[] topKFrequent(int[] nums, int k) {
+        //先统计每个数字出现的次数
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+        //最大堆
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        for (int key : map.keySet())
+            priorityQueue.add(new int[]{key, map.get(key)});
+
+        //取堆中最大的k个元素
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++)
+            res[i] = priorityQueue.poll()[0];
+        return res;
+    }
+}
